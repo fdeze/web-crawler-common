844c1061e2f5c03c2f0f36bc0c3ab8183b7ed73e
fab <fab@LAPTOP-TG7U2CSN>
2019-03-26 17:43:27 +0100

    vo

--- src/main/java/fr/fabien/webcrawler/common/model/LinkedinOfferVo.java
+++ src/main/java/fr/fabien/webcrawler/common/model/LinkedinOfferVo.java
@@ -3,6 +3,20 @@ package fr.fabien.webcrawler.common.model;
  
 public class LinkedinOfferVo extends AbstractOfferVo {
 
+	private String lieu;
+
+	public String getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+
+	@Override
+	public String toString() {
+		return "LinkedinOfferVo [lieu=" + lieu + "]";
+	}
 	
 
 }
