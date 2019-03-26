5b770255600052757e950fe239ca0558e932b5ba
fab <fab@LAPTOP-TG7U2CSN>
2019-03-26 17:50:09 +0100

    vo

--- src/main/java/fr/fabien/webcrawler/common/model/LinkedinOfferVo.java
+++ src/main/java/fr/fabien/webcrawler/common/model/LinkedinOfferVo.java
@@ -4,6 +4,8 @@ package fr.fabien.webcrawler.common.model;
 public class LinkedinOfferVo extends AbstractOfferVo {
 
 	private String lieu;
+	
+	private String entreprise;
 
 	public String getLieu() {
 		return lieu;
@@ -12,10 +14,20 @@ public class LinkedinOfferVo extends AbstractOfferVo {
 	public void setLieu(String lieu) {
 		this.lieu = lieu;
 	}
+	
+	
+
+	public String getEntreprise() {
+		return entreprise;
+	}
+
+	public void setEntreprise(String entreprise) {
+		this.entreprise = entreprise;
+	}
 
 	@Override
 	public String toString() {
-		return "LinkedinOfferVo [lieu=" + lieu + "]";
+		return "LinkedinOfferVo [lieu=" + lieu + ", entreprise=" + entreprise + "]";
 	}
 	
 
