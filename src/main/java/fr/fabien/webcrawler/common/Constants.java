c146e4cd404cde051b44a58721682d00f823c32c
fab <fab@LAPTOP-TG7U2CSN>
2019-04-09 13:29:20 +0200

    k

--- src/main/java/fr/fabien/webcrawler/common/Constants.java
+++ src/main/java/fr/fabien/webcrawler/common/Constants.java
@@ -2,7 +2,7 @@ package fr.fabien.webcrawler.common;
 
 public final class Constants {
 
-	public static String USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0";
+	public final static String USER_AGENT = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0";
 
 	private Constants() {
 	}
